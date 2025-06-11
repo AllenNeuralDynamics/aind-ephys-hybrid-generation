716c584cb7a5e3fb427304fd1a3c79909d4451cf
Alessio Buccino <alejoe9187@gmail.com>
2025-06-11 15:26:53 +0200

    Extend job env to SLURM

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -123,7 +123,10 @@ if __name__ == "__main__":
         job_dicts.append(job_dict)
     print(f"Found {len(job_dicts)} JSON job files")
 
-    si.set_global_job_kwargs(n_jobs=-1, progress_bar=False)
+    # Use CO_CPUS/SLURM_JOB_CPUS_PER_NODE env variable if available
+    N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("SLURM_JOB_CPUS_PER_NODE")
+    N_JOBS = int(N_JOBS_EXT) if N_JOBS_EXT is not None else -1
+    si.set_global_job_kwargs(dict(n_jobs=N_JOBS))
 
     templates_info = sgen.fetch_templates_database_info()
 
