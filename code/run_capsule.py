a2c65beb19f257488ca5acee54809b3640a22edb
Alessio Buccino <alejoe9187@gmail.com>
2025-06-12 14:35:07 +0200

    use SLURM_CPUS_ON_NODE instead of SLURM_JOB_CPUS_PER_NODE

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -124,8 +124,8 @@ if __name__ == "__main__":
         job_dicts.append(job_dict)
     print(f"Found {len(job_dicts)} JSON job files")
 
-    # Use CO_CPUS/SLURM_JOB_CPUS_PER_NODE env variable if available
-    N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("SLURM_JOB_CPUS_PER_NODE")
+    # Use CO_CPUS/SLURM_CPUS_ON_NODE env variable if available
+    N_JOBS_EXT = os.getenv("CO_CPUS") or os.getenv("SLURM_CPUS_ON_NODE")
     N_JOBS = int(N_JOBS_EXT) if N_JOBS_EXT is not None else -1
     si.set_global_job_kwargs(n_jobs=N_JOBS, progress_bar=False)
 
